2d2744aee1d2c7671863375a71fcb3145e61722f
Lance Carson <ldcarson@usca.edu>
2023-11-13 13:20:04 -0500

    11/13 Commit

--- js/changeLink.js
+++ js/changeLink.js
@@ -1,1 +1,26 @@
-$('body').hide().fadeIn(3000);
+//callback function example
+//$('body').hide().fadeIn(3000,function(){alert("done");});
+
+$('h1').click(function(){
+    $(this).text("Hide the image");
+    $('img').hide();
+    $('body').children('p').slideToggle();
+});
+
+//play w jquery .each function
+
+//update the image link
+$('img').each(function(){
+    var path = $(this).attr('src');
+    var newPath = "img/" + path;
+    $(this).attr('src',newPath);
+    
+    console.log(newPath);
+
+});
+
+//update all links to google
+$('a').each(function(){
+    var Goog = "https://google.com";
+    $(this).attr('href',Goog);
+});
---
+++ js/hotel.js
@@ -0,0 +1,12 @@
+//hotel registrations
+var test = {
+    hotelName: "Marriot",
+    room: 456,
+    booked: 56,
+    display: function(){
+        return this.hotelName;}
+};
+
+//Access the property
+console.log(test.room);
+console.log(test.booked);
